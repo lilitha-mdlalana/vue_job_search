62c78a8a0555c578bd59556eba1fe2c9f888288b
Lilitha Mdlalana <jranonymous517@gmail.com>
2022-05-23 18:49:47 +0200

    Add Tailwind and add Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <h1>Hello. I am in in App.vue</h1>
+  <main-nav />
 </template>
 
 <script>
+import MainNav from "@/components/MainNav.vue";
 export default {
   name: "App",
+  components: {
+    MainNav,
+  },
 };
 </script>
---
+++ src/playground.js
@@ -0,0 +1,8 @@
+//Spread syntax
+
+// const fruits = ["Apple", "Banana", "Orange"];
+// const vegetables = ["Cucumber", "Radish"];
+
+// const basket = [...fruits, ...vegetables];
+// console.log(basket);
+
