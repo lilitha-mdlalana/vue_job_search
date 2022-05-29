07e485caa2fbb8f7a00beeec48e908679aad8a81
Lilitha Mdlalana <jranonymous517@gmail.com>
2022-05-29 16:09:29 +0200

    Add Headline component and utility function

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <main-nav />
+  <hero />
 </template>
 
 <script>
 import MainNav from "@/components/MainNav.vue";
-
+import Hero from "@/components/Hero.vue";
 export default {
   name: "App",
   components: {
     MainNav,
+    Hero,
   },
 };
 </script>
