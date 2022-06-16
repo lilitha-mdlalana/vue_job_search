745a2cde4a8578826900a7f76f7296487c267f91
Lilitha Mdlalana <jranonymous517@gmail.com>
2022-06-16 23:46:25 +0200

    Add Spotlight slot  component

--- src/components/JobSearch/Hero.vue
+++ src/components/JobSearch/Hero.vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <section class="flex flex-col h-screen pt-10 pb-2">
+  <main class="flex flex-col h-screen">
+    <section class="flex flex-col pt-10 pb-2">
       <div class="grid grid-cols-12">
         <div class="col-start-1 col-span-1"></div>
         <div class="col-start-2 col-span-5">
@@ -17,18 +17,44 @@
         <div class="col-start-12 col-span-1"></div>
       </div>
     </section>
+    <spotlight class="flex flex-row justify-center pb-16">
+      <template #default="{ img, title, description }">
+        <router-link
+          to="/jobs/results"
+          class="flex flex-col mx-5 my-12 border rounded-lg w-72 bg-brand-gray-2 h-96"
+        >
+          <img class="object-contain" :src="img" />
+          <div class="h-48 px-6 py-4 mt-3">
+            <h3 class="text-lg font-medium">
+              {{ title }}
+            </h3>
+            <p class="mt-3 text-sm">
+              {{ description }}
+            </p>
+          </div>
+          <router-link
+            to="/jobs/results"
+            class="px-6 pb-4 text-sm text-brand-blue"
+          >
+            See jobs
+          </router-link>
+        </router-link>
+      </template>
+    </spotlight>
   </main>
 </template>
 
 <script>
 import HeadLine from "@/components/JobSearch/HeadLine.vue";
 import JobSearchForm from "@/components/JobSearch/JobSearchForm.vue";
+import Spotlight from "@/components/JobSearch/Spotlight.vue";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Hero",
   components: {
     HeadLine,
     JobSearchForm,
+    Spotlight,
   },
 };
 </script>
