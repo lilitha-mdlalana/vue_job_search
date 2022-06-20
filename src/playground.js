d9aaaa1e047ca6053f85c78f3b7cfb7546f0e8e8
Lilitha Mdlalana <jranonymous517@gmail.com>
2022-06-20 21:06:05 +0200

    Learn about reactivity

--- src/playground.js
+++ src/playground.js
@@ -1,11 +1,11 @@
-//arrays - order
-// objects - association
-// set - Uniqueness
+const { ref, reactive, computed } = require("vue");
 
-const numbers = new Set();
-numbers.add(5);
-numbers.add(10);
-numbers.add(15);
-numbers.add(5);
-numbers.add(10);
-console.log(numbers);
+const person = reactive({
+  name: "Boris",
+});
+
+let title = computed(() => person.name + " the Great");
+console.log(title.value);
+
+person.name = "Lilitha";
+console.log(title.value);
